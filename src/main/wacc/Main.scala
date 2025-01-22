a45e7314407e6ac1abd79f54d3f56e2db6a5d3e1
tc2323 <tc2323@ic.ac.uk>
2025-01-22 15:24:47 +0000

    Testing LabTS

--- src/main/wacc/Main.scala
+++ src/main/wacc/Main.scala
@@ -3,13 +3,7 @@ package wacc
 import parsley.{Success, Failure}
 
 def main(args: Array[String]): Unit = {
-    println("#syntax_error#")
+    println("hello WACC!")
+    println("exit:\n100")
 
-    args.headOption match {
-        case Some(expr) => parser.parse(expr) match {
-            case Success(x) => println(s"$expr = $x")
-            case Failure(msg) => println(msg)
-        }
-        case None => println("please enter an expression")
-    }
 }
