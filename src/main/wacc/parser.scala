61c68fb745a3ef19fcb88b41c639fde9057dbe95
jx1823 <jx1823@ic.ac.uk>
2025-01-23 14:00:36 +0000

    FIX: comment out some example code to ensure the running

--- src/main/wacc/parser.scala
+++ src/main/wacc/parser.scala
@@ -4,7 +4,7 @@ import parsley.{Parsley, Result}
 import parsley.expr.chain
 
 import lexer.implicits.implicitSymbol
-import lexer.{integer, fully}
+// import lexer.{integer, fully}
 
 object parser {
     def parse(input: String): Result[String, BigInt] = parser.parse(input)
@@ -12,9 +12,10 @@ object parser {
     
     private val add = (x: BigInt, y: BigInt) => x + y
     private val sub = (x: BigInt, y: BigInt) => x - y
-
+    /*
     private lazy val expr: Parsley[BigInt] =
         chain.left1(integer | "(" ~> expr <~ ")")(
             ("+" as add) | ("-" as sub)
         )
+    */
 }
