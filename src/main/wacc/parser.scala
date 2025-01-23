7dbc4cf4785b15e613b6b4edc0c9c37c06b437ff
jx1823 <jx1823@ic.ac.uk>
2025-01-23 14:20:02 +0000

    FIX: make parser now runnable without bug

--- src/main/wacc/parser.scala
+++ src/main/wacc/parser.scala
@@ -4,7 +4,7 @@ import parsley.{Parsley, Result}
 import parsley.expr.chain
 
 import lexer.implicits.implicitSymbol
-// import lexer.{integer, fully}
+import lexer.{digit, fully}
 
 object parser {
     def parse(input: String): Result[String, BigInt] = parser.parse(input)
@@ -12,10 +12,8 @@ object parser {
     
     private val add = (x: BigInt, y: BigInt) => x + y
     private val sub = (x: BigInt, y: BigInt) => x - y
-    /*
     private lazy val expr: Parsley[BigInt] =
-        chain.left1(integer | "(" ~> expr <~ ")")(
+        chain.left1(digit | "(" ~> expr <~ ")")(
             ("+" as add) | ("-" as sub)
         )
-    */
 }
