cce3fc07ff07719294bbde4ec9228fab9a3f80b9
jx1823 <jx1823@ic.ac.uk>
2025-01-23 12:02:24 +0000

    FEAT: add lexical token for <digit>, <int-sign> and <int-liter>

--- src/main/wacc/lexer.scala
+++ src/main/wacc/lexer.scala
@@ -3,6 +3,8 @@ package wacc
 import parsley.Parsley
 import parsley.token.Lexer
 import parsley.token.descriptions.*
+import parsley.character.{char, digit}
+import parsley.combinator.{many1, option}
 
 object lexer {
     private val desc = LexicalDesc.plain.copy(
@@ -10,7 +12,16 @@ object lexer {
     )
     private val lexer = Lexer(desc)
 
-    val integer = lexer.lexeme.integer.decimal
+    val digit: Parsley[Char] = digit  // single digit '0'-'9'
+    val intSign: Parsley[Char] = char('+') <|> char('-') // '+' or '-'
+    val intLiter: Parsley[BigInt] = 
+        (option(intSign) ~ many1(digit)).map {
+            case (Some(sign), digits) => 
+                BigInt((sign + digits.mkString).toInt)
+            case (None, digits)       => 
+                BigInt(digits.mkString.toInt)
+        }
+
     val implicits = lexer.lexeme.symbol.implicits
     def fully[A](p: Parsley[A]): Parsley[A] = lexer.fully(p)
 }
