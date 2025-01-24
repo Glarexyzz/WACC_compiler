43dbed270b7e96247ad58dcbd9ff265c3cf26df9
Wei Ng <wjn23@ash08.doc.ic.ac.uk>
2025-01-24 13:54:51 +0000

    FEAT: implemented testing for subcategories in 3 types. ask if you don't know how to ignore

--- src/test/wacc/wacc-tests.scala
+++ src/test/wacc/wacc-tests.scala
@@ -5,70 +5,89 @@ import scala.io.Source
 import scala.sys.process._
 import wacc.Constants._
 
-/* we want to sort the tests by their subdirectories instead of 
-  valid, invalid_syntax, invalid_semantics. The idea is that to use the switch case to sort the tests
-  "advanced" "array", "basic" "exit, "expressions" "function",  "if" "literals" "IO" "multiple" "pairs" "runtimeErr" "print" "scope" "sequence" "read"  "variables" "while"
-  */  
+/* all possible subcategories: 
+  Set ("advanced", "array", "basic", "exit", "expressions", 
+  "function", "if", "literals", "IO", "multiple", "pairs", "runtimeErr", "print", "scope", 
+  "sequence", "read",  "variables", "while")
+  */
+  
+trait WACCTestUtils {
+  val ignoredSubcategories =   Set ("advanced", "array", "basic", "exit", "expressions", 
+  "function", "if", "literals", "IO", "multiple", "pairs", "runtimeErr", "print", "scope", 
+  "sequence", "read",  "variables", "while")
 
-class ValidWACCtests extends AnyFlatSpec with Matchers {
-
-  val testRootValid = "src/test/wacc/valid"
-  val testRootSyntax = "src/test/wacc/invalid_syntax"
-  val testRootSemantics = "src/test/wacc/invalid_semantics"
-
-  val testFilesValid = getListOfWaccFiles(new File(testRootValid))
-  val testFilesSyntax = getListOfWaccFiles(new File(testRootSyntax))
-  val testFilesSemantics = getListOfWaccFiles(new File(testRootSemantics))
-
- // Valid
-  testFilesValid.foreach { file => 
-    "Valid" should s"return exit code $exitValid for $file" in {
-        val exitCode = runCompilerAndGetExitCode(file.getAbsolutePath)
-        withClue(s"File ${file.getName} failed: ") {
-          exitCode shouldBe exitValid
-      }
-    }
-
+  def getListOfWaccFiles(dir: File): Map[String, List[File]] = {
+    val subdirs = dir.listFiles.filter(_.isDirectory)
+    subdirs.map { subdir =>
+      val files = subdir.listFiles.filter(_.getName.endsWith(".wacc")).toList
+      subdir.getName -> files
+    }.toMap
   }
 
-  // Invalid Syntax
-  testFilesSyntax.foreach { file => 
-    ignore should s"return exit code $exitInvalidSyntax for $file" in {
-        val exitCode = runCompilerAndGetExitCode(file.getAbsolutePath)
-        withClue(s"File ${file.getName} failed: ") {
-          exitCode shouldBe exitInvalidSyntax
-      }
-    }
-
-  }
-
-  // Invalid Semantics
-  testFilesSemantics.foreach { file => 
-    ignore should s"return exit code $exitInvalidSemantics for $file" in {
-        val exitCode = runCompilerAndGetExitCode(file.getAbsolutePath)
-        withClue(s"File ${file.getName} failed: ") {
-          exitCode shouldBe exitInvalidSemantics
-      }
-    }
-  }
-
-  private def getListOfWaccFiles(dir: File): List[File] = {
-    val files = dir.listFiles
-    if (files != null) {
-      files.toList.flatMap { file =>
-        if (file.isDirectory) getListOfWaccFiles(file)
-        else if (file.getName.endsWith(".wacc")) List(file)
-        else Nil
-      }
-    } else {
-      Nil
-    }
-  }
-
-  private def runCompilerAndGetExitCode(filePath: String): Int = {
+  def runCompilerAndGetExitCode(filePath: String): Int = {
     val command = s"scala shebang . $filePath"
     val process = Process(command)
     val exitValue = process.!
     exitValue
+  }
+}
+
+class ValidTest extends AnyFlatSpec with Matchers with WACCTestUtils {
+  val testRootValid = new File("src/test/wacc/valid")
+  val testFiles = getListOfWaccFiles(testRootValid)
+
+  for ((subcat, files) <- testFiles) {
+    if (ignoredSubcategories.contains(subcat)) {
+      ignore should s"return exit code $exitValid for subcategory $subcat" in {}
+    } else {
+      subcat should s"return exit code $exitValid for all files" in {
+        files.foreach { file =>
+          val exitCode = runCompilerAndGetExitCode(file.getAbsolutePath)
+          withClue(s"File ${file.getName} failed: ") {
+            exitCode shouldBe exitValid
+          }
+        }
+      }
+    }
+  }
+}
+
+class InvalidSyntaxTest extends AnyFlatSpec with Matchers with WACCTestUtils {
+  val testRootSyntax = new File("src/test/wacc/invalid_syntax")
+  val testFiles = getListOfWaccFiles(testRootSyntax)
+
+  for ((subcat, files) <- testFiles) {
+    if (ignoredSubcategories.contains(subcat)) {
+      ignore should s"return exit code $exitInvalidSyntax for subcategory $subcat" in {}
+    } else {
+      subcat should s"return exit code $exitInvalidSyntax for all files" in {
+        files.foreach { file =>
+          val exitCode = runCompilerAndGetExitCode(file.getAbsolutePath)
+          withClue(s"File ${file.getName} failed: ") {
+            exitCode shouldBe exitInvalidSyntax
+          }
+        }
+      }
+    }
+  }
+}
+
+class InvalidSemanticsTest extends AnyFlatSpec with Matchers with WACCTestUtils {
+  val testRootSemantics = new File("src/test/wacc/invalid_semantics")
+  val testFiles = getListOfWaccFiles(testRootSemantics)
+
+  for ((subcat, files) <- testFiles) {
+    if (ignoredSubcategories.contains(subcat)) {
+      ignore should s"return exit code $exitInvalidSemantics for subcategory $subcat" in {}
+    } else {
+      subcat should s"return exit code $exitInvalidSemantics for all files" in {
+        files.foreach { file =>
+          val exitCode = runCompilerAndGetExitCode(file.getAbsolutePath)
+          withClue(s"File ${file.getName} failed: ") {
+            exitCode shouldBe exitInvalidSemantics
+          }
+        }
+      }
+    }
   }
 }
