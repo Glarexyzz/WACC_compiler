3056c92b870abee037aec3173cd87053395fcaf2
jx1823 <jx1823@ic.ac.uk>
2025-01-24 20:38:54 +0000

    CHORE: add generic ParserBridge for simpler usage of each case class

--- src/main/wacc/Syntax.scala
+++ src/main/wacc/Syntax.scala
@@ -5,13 +5,27 @@ import parsley.Parsley
 
 sealed trait Expr
 
+
 case class IntLiteral(value: BigInt) extends Expr
+object IntLiteral extends generic.ParserBridge1[BigInt, Expr]
+
 case class BoolLiteral(value: Boolean) extends Expr
+object BoolLiteral extends generic.ParserBridge1[Boolean, Expr]
+
 case class CharLiteral(value: Char) extends Expr
+object CharLiteral extends generic.ParserBridge1[Char, Expr]
+
 case class StrLiteral(value: String) extends Expr
-case object PairLiteral extends Expr  // Potential problematic? need to recheck
+object StrLiteral extends generic.ParserBridge1[String, Expr]
+
+case object PairLiteral extends Expr
+
 case class Identifier(name: String) extends Expr
+object Identifier extends generic.ParserBridge1[String, Expr]
+
 case class ArrayElem(name: String, indices: List[Expr]) extends Expr
+object ArrayElem extends generic.ParserBridge2[String, List[Expr], ArrayElem]
+
 case class UnaryOp(op: UnaryOperator, expr: Expr) extends Expr
 case class BinaryOp(left: Expr, op: BinaryOperator, right: Expr) extends Expr
 //case class ParenExpr(expr: Expr) extends Expr
