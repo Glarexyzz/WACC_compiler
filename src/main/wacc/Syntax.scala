d170e8e5fd84f86003202046a83694af6608c635
Tabeer Asif <tabeerasif5@gmail.com>
2025-01-24 12:08:50 +0000

    done all operators in table 1

--- src/main/wacc/Syntax.scala
+++ src/main/wacc/Syntax.scala
@@ -1,7 +1,4 @@
-// Define AST types for expressions
-sealed trait Expr
-case class Val(value: Any) extends Expr
-case class Var(name: String) extends Expr
-case class UnaryOp(op: String, expr: Expr) extends Expr
-case class BinaryOp(op: String, left: Expr, right: Expr) extends Expr
-case class ArrayElem(name: String, indices: List[Expr]) extends Expr
+sealed trait Expr extends RValue
+sealed trait LValue
+sealed trait RValue 
+case class Identifier extends Expr, LValue, RValue
--- src/main/wacc/parser.scala
+++ src/main/wacc/parser.scala
@@ -10,11 +10,23 @@ object parser {
     def parse(input: String): Result[String, BigInt] = parser.parse(input)
     //private val parser = fully(expr)
     
+    // Operators
     private val add = (x: BigInt, y: BigInt) => x + y
     private val sub = (x: BigInt, y: BigInt) => x - y
     private val mul = (x: BigInt, y: BigInt) => x * y
     private val div = (x: BigInt, y: BigInt) => x / y
     private val mod = (x: BigInt, y: BigInt) => x % y
+    private val and = (x: Boolean, y: Boolean) => x && y
+    private val or = (x: Boolean, y: Boolean) => x || y
+    private val eq = (x: Any, y: Any) => x == y
+    private val neq = (x: Any, y: Any) => x != y
+    private val gt = (x: BigInt, y: BigInt) => x > y
+    private val gte = (x: BigInt, y: BigInt) => x >= y
+    private val lt = (x: BigInt, y: BigInt) => x < y
+    private val lte = (x: BigInt, y: BigInt) => x <= y
+
+    private val not = (x: Boolean) => !x
+    private val negate = (x: BigInt) => -x
     /*
     private lazy val expr: Parsley[BigInt] =
         chain.left1(digit | "(" ~> expr <~ ")")(
