eee4bab79c4d96f523162bea2c0b6328633ee697
Tabeer Asif <tabeerasif5@gmail.com>
2025-01-24 12:28:47 +0000

    CHORE: adding to AST structure

--- src/main/wacc/Syntax.scala
+++ src/main/wacc/Syntax.scala
@@ -1,4 +1,13 @@
-sealed trait Expr extends RValue
-sealed trait LValue
-sealed trait RValue 
-case class Identifier extends Expr, LValue, RValue
+package wacc
+
+sealed trait Expr
+case class IntLiteral(value: BigInt) extends Expr
+case class BoolLiteral(value: Boolean) extends Expr
+case class CharLiteral(value: Char) extends Expr
+case class StrLiteral(value: String) extends Expr
+case class PairLiteral() extends Expr
+case class Identifier(name: String) extends Expr
+case class ArrayElem(name: String, indices: List[Expr]) extends Expr
+case class UnaryOp(op: String, expr: Expr) extends Expr
+case class BinaryOp(op: String, left: Expr, right: Expr) extends Expr
+case class ParenExpr(expr: Expr) extends Expr
--- src/main/wacc/parser.scala
+++ src/main/wacc/parser.scala
@@ -2,9 +2,11 @@ package wacc
 
 import parsley.{Parsley, Result}
 import parsley.expr.chain
+import parsley.expr.{precedence, Ops, InfixL, InfixN, InfixR, Prefix}
 
 import lexer.implicits.implicitSymbol
-import lexer.{digit, fully}
+import lexer.{digit, fully, intLiter, boolLiter, charLiter, strLiter, pairLiter, ident}
+
 
 object parser {
     def parse(input: String): Result[String, BigInt] = parser.parse(input)
@@ -28,9 +30,31 @@ object parser {
     private val not = (x: Boolean) => !x
     private val negate = (x: BigInt) => -x
     /*
+    private lazy val atom: Parsley[Expr] =
+    intLiter.map(IntLiteral) |
+    boolLiter.map(BoolLiteral) |
+    charLiter.map(CharLiteral) |
+    strLiter.map(StrLiteral) |
+    pairLiter.map(_ => PairLiteral()) |
+    ident.map(Identifier) |
+    arrayElem.map { case (name, indices) => ArrayElem(name, indices) } |
+    "(" ~> expr.map(ParenExpr) <~ ")"
+    */
+    /*
     private lazy val expr: Parsley[BigInt] =
         chain.left1(digit | "(" ~> expr <~ ")")(
             ("+" as add) | ("-" as sub)
         )
     */
+    /*
+    private lazy val expr: Parsley[Expr] = precedence[Expr](
+        Ops(Prefix)(not, "!", "len", "ord", "chr"),        // Unary operators
+        Ops(InfixL)(mul, "*", "/", "%"),                 // Multiplicative
+        Ops(InfixL)(add, "+", "-"),                      // Additive
+        Ops(InfixN)(gt, ">", gte, ">=", lt, "<", lte, "<="), // Relational
+        Ops(InfixN)(eq, "==", neq, "!="),                // Equality
+        Ops(InfixR)(and, "&&"),                          // Logical AND
+        Ops(InfixR)(or, "||")                            // Logical OR
+    )
+    */    
 }
