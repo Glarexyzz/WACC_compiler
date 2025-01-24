bb08aa19aa963419da13fe72f3044a72fb5a8975
jx1823 <jx1823@ic.ac.uk>
2025-01-24 20:41:00 +0000

    STYLE: removed commented code

--- src/main/wacc/parser.scala
+++ src/main/wacc/parser.scala
@@ -14,55 +14,6 @@ object parser {
     def parse(input: String): Result[String, BigInt] = parser.parse(input)
     //private val parser = fully(expr)
 
-    /*   
-    //  Binary Operators
-    private val add = (x: BigInt, y: BigInt) => x + y
-    private val sub = (x: BigInt, y: BigInt) => x - y
-    private val mul = (x: BigInt, y: BigInt) => x * y
-    private val div = (x: BigInt, y: BigInt) => x / y
-    private val mod = (x: BigInt, y: BigInt) => x % y
-    private val and = (x: Boolean, y: Boolean) => x && y
-    private val or = (x: Boolean, y: Boolean) => x || y
-    private val eq = (x: Any, y: Any) => x == y
-    private val neq = (x: Any, y: Any) => x != y
-    private val gt = (x: BigInt, y: BigInt) => x > y
-    private val gte = (x: BigInt, y: BigInt) => x >= y
-    private val lt = (x: BigInt, y: BigInt) => x < y
-    private val lte = (x: BigInt, y: BigInt) => x <= y
-
-    // Unary Operators
-    private val not = (x: Boolean) => !x
-    private val negate = (x: BigInt) => -x
-
-    private lazy val atom: Parsley[Expr] =
-    intLiter.map(IntLiteral) |
-    boolLiter.map(BoolLiteral) |
-    charLiter.map(CharLiteral) |
-    strLiter.map(StrLiteral) |
-    pairLiter.map(_ => PairLiteral()) |
-    ident.map(Identifier) |
-    arrayElem.map { case (name, indices) => ArrayElem(name, indices) } |
-    "(" ~> expr.map(ParenExpr) <~ ")"
-    */
-    /*
-    private lazy val expr: Parsley[BigInt] =
-        chain.left1(digit | "(" ~> expr <~ ")")(
-            ("+" as add) | ("-" as sub)
-        )
-    */
-    /*
-    private lazy val expr: Parsley[Expr] = precedence[Expr](
-        Ops(Prefix)(not, "!", "len", "ord", "chr"),        // Unary operators
-        Ops(InfixL)(mul, "*", "/", "%"),                 // Multiplicative
-        Ops(InfixL)(add, "+", "-"),                      // Additive
-        Ops(InfixN)(gt, ">", gte, ">=", lt, "<", lte, "<="), // Relational
-        Ops(InfixN)(eq, "==", neq, "!="),                // Equality
-        Ops(InfixR)(and, "&&"),                          // Logical AND
-        Ops(InfixR)(or, "||")                            // Logical OR
-    )
-    */
-
-
     //  Parser for expressions  
 
     //  Unary Operators (Highest Precedence)
