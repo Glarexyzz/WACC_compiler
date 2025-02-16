e24bd558e3c16df1dbd8b89b74f63780f4347cbd
jx1823 <jx1823@ic.ac.uk>
2025-02-16 14:40:19 +0000

    DOC: made a list of brief plans to start things up

--- src/main/wacc/CodeGen.scala
+++ src/main/wacc/CodeGen.scala
@@ -1,5 +1,9 @@
 package wacc
-
+/*
+1. Define IR representations for AST
+2. Implement code generation for expressions and statements
+3. Write an AArch64 assembly backend (to plan in detail more later)
+*/
 object CodeGen {
   def compile(prog: Any, filepath: String): Unit = {
     println("Compiling...")
