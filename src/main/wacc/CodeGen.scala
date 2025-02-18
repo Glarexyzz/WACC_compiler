1abdcbe75e14c6c2d92975a6e6ee8f0539a7a6ac
jx1823 <jx1823@ic.ac.uk>
2025-02-18 22:13:31 +0000

    FEAT: add functions for transfer parsed wacc code into IR

--- src/main/wacc/CodeGen.scala
+++ src/main/wacc/CodeGen.scala
@@ -8,4 +8,59 @@ object CodeGen {
   def compile(prog: Any, filepath: String): Unit = {
     println("Compiling...")
   }
+
+  def generateIR(stmt: Stmt): List[IRInstr] = stmt match {
+    case DeclAssignStmt(varType, name, RValue.RExpr(expr)) =>
+      generateIR(expr) ++ List(IRStore(name, "R0"))  // Store result in variable
+    
+    case AssignStmt(LValue.LName(name), RValue.RExpr(expr)) =>
+      generateIR(expr) ++ List(IRStore(name, "R0"))  // Assign to variable
+    
+    case PrintStmt(expr) =>
+      generateIR(expr) ++ List(IRPrint("R0"))  // Print result
+    
+    case IfStmt(cond, thenStmt, elseStmt) =>
+      val thenIR = generateIR(thenStmt)
+      val elseIR = generateIR(elseStmt)
+      val endLabel = freshLabel("end_if")
+
+      generateIR(cond) ++
+        List(IRJumpCond("EQ", endLabel)) ++
+        thenIR ++
+        List(IRJump(endLabel)) ++
+        elseIR ++
+        List(IRLabel(endLabel))
+      
+    case SeqStmt(left, right) =>
+      generateIR(left) ++ generateIR(right)
+
+    case _ => throw new Exception(s"Unsupported statement: $stmt")
+  }
+
+  def generateIR(expr: Expr): List[IRInstr] = expr match {
+    case IntLiteral(value) => List(IRStore("R0", value.toString))  
+    case BinaryOp(left, op, right) =>
+      val leftIR = generateIR(left)
+      val rightIR = generateIR(right)
+      leftIR ++ rightIR ++ List(IRBinaryOp(opToIR(op), "R0", "R1", "R2"))
+    case Identifier(name) => List(IRLoad("R0", name))
+    case _ => throw new Exception(s"Unsupported expression: $expr")
+  }
+
+  def opToIR(op: BinaryOperator): String = op match {
+    case BinaryOperator.Add => "ADD"
+    case BinaryOperator.Subtract => "SUB"
+    case BinaryOperator.Multiply => "MUL"
+    case BinaryOperator.Divide => "DIV"
+    case BinaryOperator.Modulus => "MOD"
+    case BinaryOperator.Equal => "CMP"
+    case BinaryOperator.NotEqual => "CMP"
+    case _ => throw new Exception("Unsupported operator!")
+  }
+
+  var labelCounter = 0
+  def freshLabel(base: String): String = {
+    labelCounter += 1
+    s"${base}_$labelCounter"
+  }
 }
