7c848bd3be8866fd377aa0db80f5d51f221eddf5
jx1823 <jx1823@ic.ac.uk>
2025-02-19 03:32:25 +0000

    FIX: redo the aarch assembly generation

--- src/main/wacc/AArch64.scala
+++ src/main/wacc/AArch64.scala
@@ -3,78 +3,74 @@ package wacc
 object AArch64Gen {
   
   def generateAssembly(irList: List[IRInstr]): String = {
-    val asm = irList.flatMap(instrToAsm).mkString("\n")
-    s"""
-    .global main
-    .text
-    main:
-    $asm
-    mov x8, #93      // Exit syscall
-    svc 0            // Call kernel
-    """
+    val asm = new StringBuilder
+    asm.append(".global _start\n")
+    asm.append(".section .text\n")
+
+    asm.append("_start:\n")
+
+    irList.foreach { instr =>
+      asm.append(translateInstr(instr) + "\n")
+    }
+
+    asm.append("  mov x8, #93  // syscall: exit\n")
+    asm.append("  mov x0, #0   // return code 0\n")
+    asm.append("  svc #0       // invoke syscall\n")
+
+    asm.toString()
   }
 
-  def instrToAsm(instr: IRInstr): List[String] = instr match {
-    case IRLoad(dest, src) =>
-      List(s"LDR $dest, [$src]")  // Load from memory
+  private def translateInstr(instr: IRInstr): String = instr match {
+    // ✅ Memory Operations
+    case IRLoad(dest, src) => s"  ldr $dest, [$src]"
+    case IRStore(dest, src) => s"  str $src, [$dest]"
+    case IRAlloc(name, size) =>
+      s"  sub sp, sp, #$size\n  mov $name, sp"
 
-    case IRStore(dest, src) =>
-      List(s"STR $src, [$dest]")  // Store to memory
+    // ✅ Arithmetic Operations
+    case IRBinaryOp(BinaryOperator.Add, dest, left, right) => s"  add $dest, $left, $right"
+    case IRBinaryOp(BinaryOperator.Subtract, dest, left, right) => s"  sub $dest, $left, $right"
+    case IRBinaryOp(BinaryOperator.Multiply, dest, left, right) => s"  mul $dest, $left, $right"
+    case IRBinaryOp(BinaryOperator.Divide, dest, left, right) => s"  sdiv $dest, $left, $right"
+    case IRBinaryOp(BinaryOperator.Modulus, dest, left, right) =>
+      s"  sdiv x9, $left, $right\n  msub $dest, x9, $right, $left"
 
-    case IRBinaryOp("ADD", dest, left, right) =>
-      List(s"ADD $dest, $left, $right")  // Addition
-    
-    case IRBinaryOp("SUB", dest, left, right) =>
-      List(s"SUB $dest, $left, $right")  // Subtraction
+    // ✅ Unary Operations
+    case IRUnaryOp(UnaryOperator.Not, dest, src) => s"  mvn $dest, $src"
+    case IRUnaryOp(UnaryOperator.Negate, dest, src) => s"  neg $dest, $src"
 
-    case IRBinaryOp("MUL", dest, left, right) =>
-      List(s"MUL $dest, $left, $right")  // Multiplication
-
-    case IRBinaryOp("DIV", dest, left, right) =>
-      List(
-        s"SDIV $dest, $left, $right"  // Division
-      )
-
-    case IRBinaryOp("MOD", dest, left, right) =>
-      List(
-        s"SDIV X9, $left, $right", // X9 = left / right
-        s"MSUB $dest, X9, $right, $left" // dest = left - (X9 * right)
-      )
-
-    case IRPrint(value) =>
-      List(
-        s"MOV X0, $value",
-        "BL printf"
-      )
-
-    case IRRead(dest) =>
-      List(
-        s"BL scanf",
-        s"STR X0, [$dest]"
-      )
-
-    case IRJump(label) =>
-      List(s"B $label")  // Unconditional jump
-
-    case IRJumpCond(cond, label) =>
-      List(
-        s"CMP X0, #0",
-        s"B$cond $label"
-      )  // Conditional jump
-
-    case IRLabel(name) =>
-      List(s"$name:")
-
+    // ✅ Function Calls & Returns
     case IRCall(func, args) =>
-      args.zipWithIndex.map { case (arg, i) => s"MOV X$i, $arg" } ++
-        List(s"BL $func")
+      val argSetup = args.zipWithIndex.map { case (arg, i) => s"  mov x${i}, $arg" }.mkString("\n")
+      s"$argSetup\n  bl $func"
 
-    case IRReturn(value) =>
-      List(
-        s"MOV X0, $value",
-        "RET"
-      )
+    case IRReturn(value) => s"  mov x0, $value\n  ret"
 
-    case _ => throw new Exception(s"Unknown IR instruction: $instr")
+    // ✅ Control Flow
+    case IRJump(label) => s"  b $label"
+    case IRJumpCond(cond, label) => s"  cmp x0, x1\n  b.ne $label"
+    case IRLabel(name) => s"$name:"
+
+    // ✅ Input & Output
+    case IRPrint(value) => s"  mov x0, $value\n  bl print_string"
+    case IRPrintln(value) => s"  mov x0, $value\n  bl print_string\n  bl print_newline"
+    case IRRead(dest) => s"  mov x0, $dest\n  bl read_integer"
+
+    // ✅ Pairs & Arrays
+    case IRNewPair(dest, left, right) =>
+      s"  mov x0, #16\n  bl malloc\n  str $left, [x0]\n  str $right, [x0, #8]\n  mov $dest, x0"
+
+    case IRPairElem(dest, src, isFirst) =>
+      val offset = if (isFirst) "0" else "8"
+      s"  ldr $dest, [$src, #$offset]"
+
+    case IRArrayLoad(dest, name, index) =>
+      s"  ldr $dest, [$name, $index, LSL #3]"
+
+    case IRArrayStore(name, index, value) =>
+      s"  str $value, [$name, $index, LSL #3]"
+
+    case IRFree(dest) =>
+      s"  mov x0, $dest\n  bl free"
   }
 }
